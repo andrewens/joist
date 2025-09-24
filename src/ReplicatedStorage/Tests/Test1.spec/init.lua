6153b47fb92506e0bec5cbe12647a76d75dffa19
andrewens <andrewens4@gmail.com>
2025-09-23 19:39:58 -0600

    Rojo init

---
+++ src/ReplicatedStorage/Tests/Test1.spec/init.lua
@@ -0,0 +1,10 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Joist = require(ReplicatedStorage.Joist)
+
+return function()
+    describe("Sup", function()
+        it("Does something", function()
+        
+        end)
+    end)
+end
