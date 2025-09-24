43329dda3f6a79b2f2d1d54227078154839a388b
andrewens <andrewens4@gmail.com>
2025-09-24 14:07:12 -0600

    Joist can load tests and APIs to test

--- src/ReplicatedStorage/Tests/Test1.spec/init.lua
+++ src/ReplicatedStorage/Tests/Test1.spec/init.lua
@@ -1,10 +1,17 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Joist = require(ReplicatedStorage.Joist)
 
+local MockTestsFolder = script.MockTests
+local MockApi = script.MockApi
+
 return function()
     describe("Sup", function()
         it("Does something", function()
-        
+            local Output = Joist.runTests(MockApi, MockTestsFolder, {
+                OutputMode = "ReturnTable",
+            })
+
+            --assert(Output[1] == "")
         end)
     end)
 end
